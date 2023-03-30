4e411ad7741200e92cc218e6f23d8dd6219642e5
Thomas Vy <vythomas97@gmail.com>
2023-03-29 22:48:37 -0600

    index buffer abstraction

--- include/renderer/IndexBuffer.h
+++ include/renderer/IndexBuffer.h
@@ -1,12 +1,14 @@
 #pragma once
 #include <cstdint>
+#include <span>
 #include "graphics_api/IGraphicsApi.h"
 
 class IndexBuffer {
 public:
-	IndexBuffer(const uint32_t* data, size_t count, graphics_api::IGraphicsApi* graphicsApi);
+	IndexBuffer(graphics_api::IGraphicsApi* graphicsApi);
 	~IndexBuffer();
 
+	void UploadData(span<uint32_t> data);
 	uint32_t GetCount() const;
 	uint32_t GetId() const { return m_bufferID; }
 private:
--- src/renderer/IndexBuffer.cpp
+++ src/renderer/IndexBuffer.cpp
@@ -1,11 +1,10 @@
 #include "IndexBuffer.h"
 #include "math/MatrixAndVectorMath.h"
-IndexBuffer::IndexBuffer(const uint32_t* data, size_t count, ::graphics_api::IGraphicsApi* graphicsApi)
+IndexBuffer::IndexBuffer(::graphics_api::IGraphicsApi* graphicsApi)
     : m_graphicsApi(graphicsApi)
     , m_bufferID{m_graphicsApi->GenerateBuffers()}
-    , m_count(count)
+    , m_count{0}
 {
-    m_graphicsApi->UploadBufferData(m_bufferID, data, count*sizeof(uint32_t), ::graphics_api::IGraphicsApi::BufferType::Index);
 }
 
 IndexBuffer::~IndexBuffer()
@@ -18,3 +17,8 @@ uint32_t IndexBuffer::GetCount() const
     return static_cast<uint32_t>(m_count);
 }
 
+void IndexBuffer::UploadData(span<uint32_t> data)
+{
+    m_count = data.size();
+	m_graphicsApi->UploadBufferData(m_bufferID, data, data.size_bytes(), ::graphics_api::IGraphicsApi::BufferType::Index);
+}
